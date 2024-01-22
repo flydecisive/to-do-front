d965dc1a16801448866b2e00c73b9fe37e161aef
Maksim <45427901+flydecisive@users.noreply.github.com>
2024-01-22 15:26:00 +0300

    добавляет поиск

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import "../styles/style.scss";
-// import javascriptLogo from './javascript.svg'
 import { Header } from "./components/header/header";
+import { SearchSection } from "./components/search_section/search_section";
 import { getTodos, addTodo } from "./api/api";
 
 const header = new Header("#app");
+const searchSection = new SearchSection("#app");
+
 header.addHeader();
+searchSection.addSearchSection();
 
 document.addEventListener("DOMContentLoaded", () => {
   getTodos().then((responseData) => {
