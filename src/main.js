08060ee469eda6c9db8d0355a1a46c27cf714c84
Maksim <45427901+flydecisive@users.noreply.github.com>
2024-01-22 22:36:19 +0300

    добавляет компонент задач

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import "../styles/style.scss";
 import { Header } from "./components/header/header";
 import { SearchSection } from "./components/search_section/search_section";
+import { Todos } from "./components/todos/todos";
 import { getTodos, addTodo } from "./api/api";
 
 const header = new Header("#app");
 const searchSection = new SearchSection("#app");
+const todos = new Todos("#app", ["Тест", "Тест 2"]);
 
 header.addHeader();
 searchSection.addSearchSection();
+todos.addTodos();
 
 document.addEventListener("DOMContentLoaded", () => {
   getTodos().then((responseData) => {
